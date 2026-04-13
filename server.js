3a51746a7833c0e938964325f8239f4ae3593ecd
loromaikol9-web <loromaikol9@gmail.com>
2026-04-12 21:51:24 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
-
-
 const express = require("express");
 const path = require("path");
 
 const app = express();
 
-// Servir archivos estáticos
+app.use(express.json());
 app.use(express.static(__dirname));
 
-// Ruta principal
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+// Login fake (demo)
+app.post("/login", (req, res) => {
+  const { user, pass } = req.body;
+
+  if (user === "admin" && pass === "1234") {
+    res.json({ success: true });
+  } else {
+    res.json({ success: false });
+  }
 });
 
-const PORT = process.env.PORT || 3000;
+// Generador contraseña
+app.get("/api/password", (req, res) => {
+  let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let pass = "";
+  for (let i = 0; i < 10; i++) {
+    pass += chars[Math.floor(Math.random() * chars.length)];
+  }
+  res.json({ password: pass });
+});
+
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
   console.log("Servidor listo 🚀");
