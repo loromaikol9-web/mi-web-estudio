a8a7cb5d42c69ab8e568b60d12564067f9c17935
loromaikol9-web <loromaikol9@gmail.com>
2026-04-12 21:35:39 -0500

    Update server to use environment port variable

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 const express = require("express");
 const app = express();
 
@@ -5,6 +6,6 @@ app.get("/", (req, res) => {
   res.send("Backend funcionando 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Servidor listo en puerto 3000");
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor listo");
 });
