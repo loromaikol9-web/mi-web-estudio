d2b52364999b907bff742025336af0083d11d8a3
loromaikol9-web <loromaikol9@gmail.com>
2026-04-12 21:28:42 -0500

    Refactor server.js for static file serving

--- server.js
+++ server.js
@@ -1,9 +1,17 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Backend funcionando 🚀");
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(3000, () => console.log("Servidor listo en puerto 3000"));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor listo en puerto " + PORT);
+});
 
